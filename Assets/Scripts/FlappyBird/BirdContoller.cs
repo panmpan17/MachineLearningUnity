889e5fd3226a6ea0fb1c8aa4932a256f454fdfd6
Michael Pan <panmpan@gmail.com>
2021-03-11 14:46:28 +0800

    Add basic player control flappy bird

---
+++ Assets/Scripts/FlappyBird/BirdContoller.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdContoller : MonoBehaviour
+{
+    public float jumpVelocity = 3;
+
+    public float upwardAngularSpeed = 360;
+    public float upwardStopAngle;
+    private bool upward;
+    public float downwardAngularSpeed = -180;
+
+    protected new Rigidbody2D rigidbody2D;
+    private Vector3 initialPosition;
+
+
+    private void Awake() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.simulated = false;
+
+        initialPosition = transform.position;
+    }
+
+    private void OnEnable() {
+        transform.position = initialPosition;
+        rigidbody2D.simulated = true;
+        rigidbody2D.velocity = new Vector2(0, jumpVelocity);
+        rigidbody2D.angularVelocity = downwardAngularSpeed;
+        transform.rotation = Quaternion.Euler(0, 0, upwardStopAngle);
+    }
+
+    void Update()
+    {
+        UpdateRotation();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (rigidbody2D.simulated) Jump();
+            else ResetGame();
+        }
+    }
+
+    protected void UpdateRotation()
+    {
+        if (upward)
+        {
+            if (transform.rotation.eulerAngles.z >= upwardStopAngle)
+            {
+                upward = false;
+                rigidbody2D.angularVelocity = downwardAngularSpeed;
+            }
+        }
+    }
+
+    protected void Jump()
+    {
+        rigidbody2D.velocity = new Vector2(0, jumpVelocity);
+        rigidbody2D.angularVelocity = upwardAngularSpeed;
+
+        upward = true;
+    }
+
+    public void ResetGame()
+    {
+        transform.position = initialPosition;
+        rigidbody2D.simulated = true;
+        rigidbody2D.velocity = new Vector2(0, jumpVelocity);
+
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        rigidbody2D.angularVelocity = upwardAngularSpeed;
+        upward = true;
+    }
+
+    protected virtual void OnCollisionEnter2D(Collision2D other)
+    {
+        rigidbody2D.simulated = false;
+        GameControl.ins.GameOver();
+        enabled = false;
+    }
+}
---
+++ Assets/Scripts/FlappyBird/GameControl.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class GameControl : MonoBehaviour
+{
+    static public GameControl ins;
+
+    [SerializeField]
+    private GameObject[] groundSetPrefabs;
+
+    [SerializeField]
+    private Vector3 spawnPosition;
+    [SerializeField]
+    private float distroyX;
+
+    [SerializeField]
+    protected float scoreX;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float spawnGap;
+    private float spawnTimer;
+
+    [SerializeField]
+    private float spawnMinY, spawnMaxY;
+
+    [SerializeField]
+    private BirdContoller birdContoller;
+
+    [SerializeField]
+    protected GameObject startText;
+
+    [SerializeField]
+    protected TextMeshProUGUI scoreText;
+    private int score;
+
+    protected List<GameObject> grounds = new List<GameObject>();
+
+    protected virtual void Awake() {
+        ins = this;
+        birdContoller.enabled = false;
+    }
+
+    void Update()
+    {
+        if (!birdContoller.enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) ResetGame();
+        }
+        else UpdateGround();
+    }
+
+    protected void UpdateGround()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnGap)
+            SpawnGround();
+
+        for (int i = 0; i < grounds.Count; i++)
+        {
+            Vector3 position = grounds[i].transform.position;
+            bool cross = position.x <= scoreX;
+
+            position.x += moveSpeed * Time.deltaTime;
+
+            if (!cross && position.x <= scoreX)
+            {
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+
+            if (position.x <= distroyX)
+            {
+                Destroy(grounds[i]);
+                grounds.RemoveAt(i);
+                i--;
+                break;
+            }
+
+            grounds[i].transform.position = position;
+        }
+    }
+
+    protected void SpawnGround()
+    {
+        spawnTimer = 0;
+
+        int index = Random.Range(0, groundSetPrefabs.Length);
+        GameObject newGround = Instantiate(groundSetPrefabs[index]);
+
+        Vector3 position = spawnPosition;
+        position.y = Random.Range(spawnMinY, spawnMaxY);
+        newGround.transform.position = position;
+
+        grounds.Add(newGround);
+    }
+
+    public virtual void GameOver()
+    {
+        startText.SetActive(true);
+    }
+
+    public virtual void ResetGame()
+    {
+        startText.SetActive(false);
+        if (birdContoller != null) birdContoller.enabled = true;
+        score = 0;
+        scoreText.text = "0";
+
+        while (grounds.Count > 0)
+        {
+            Destroy(grounds[0]);
+            grounds.RemoveAt(0);
+        }
+
+        SpawnGround();
+    }
+}
