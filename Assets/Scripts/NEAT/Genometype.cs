86b5bdd01505f45453609dffb65a3ed3d44442a0
Michael Pan <panmpan@gmail.com>
2021-03-12 09:46:32 +0800

    Give Genometype more choice

--- Assets/Scripts/NEAT/Genometype.cs
+++ Assets/Scripts/NEAT/Genometype.cs
@@ -2,67 +2,108 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Genometype
+
+namespace NEAT
 {
-    public NodeGenes[] nodeGenes;
-    public ConnectionGenens[] connectionGenes;
-
-    public Genometype(NodeGenes[] _nodeGenes, ConnectionGenens[] _connectionGenes)
+    public class Genometype
     {
-        nodeGenes = _nodeGenes;
-        connectionGenes = _connectionGenes;
-    }
+        public NodeGenes[] nodeGenes;
+        public ConnectionGenens[] connectionGenes;
 
-    public Genometype Clone()
-    {
-        NodeGenes[] newNodeGenes = new NodeGenes[nodeGenes.Length];
-        for (int i = 0; i < nodeGenes.Length; i++)
+        public Genometype(NodeGenes[] _nodeGenes, ConnectionGenens[] _connectionGenes)
         {
-            newNodeGenes[i] = nodeGenes[i];
+            nodeGenes = _nodeGenes;
+            connectionGenes = _connectionGenes;
         }
 
-        ConnectionGenens[] newConnectionGenes = new ConnectionGenens[connectionGenes.Length];
-        for (int i = 0; i < connectionGenes.Length; i++)
+        public Genometype Clone()
         {
-            newConnectionGenes[i] = connectionGenes[i];
+            NodeGenes[] newNodeGenes = new NodeGenes[nodeGenes.Length];
+            for (int i = 0; i < nodeGenes.Length; i++)
+            {
+                newNodeGenes[i] = nodeGenes[i];
+            }
+
+            ConnectionGenens[] newConnectionGenes = new ConnectionGenens[connectionGenes.Length];
+            for (int i = 0; i < connectionGenes.Length; i++)
+            {
+                newConnectionGenes[i] = connectionGenes[i];
+            }
+
+            return new Genometype(newNodeGenes, newConnectionGenes);
         }
 
-        return new Genometype(newNodeGenes, newConnectionGenes);
-    }
-
-    #region Node and Connection
-    public struct NodeGenes
-    {
-        public float value;
-        public Types type;
-
-        public enum Types {
-            Sensor,
-            Output,
-            Hidden,
-        }
-
-        public NodeGenes(float _value, Types _type)
+        private void InputValueIntoNode(NodeGenes node, float value)
         {
-            value = _value;
-            type = _type;
+            switch (node.operatorType)
+            {
+                case NodeGenes.OperatorType.Multiply:
+                    node.value *= value;
+                    break;
+                case NodeGenes.OperatorType.Plus:
+                    node.value += value;
+                    break;
+            }
         }
-    }
 
-    public struct ConnectionGenens
-    {
-        public int inputNodeIndex;
-        public int outputNodeIndex;
-        public float weight;
-        public bool enabled;
-
-        public ConnectionGenens(int _inputNodeIndex, int _outputNodeIndex, float _weight, bool _enabled=true)
+        #region Node and Connection
+        public struct NodeGenes
         {
-            inputNodeIndex = _inputNodeIndex;
-            outputNodeIndex = _outputNodeIndex;
-            weight = _weight;
-            enabled = _enabled;
+            public Types type;
+            public float value;
+            public OperatorType operatorType;
+            public OutputMode outputMode;
+
+            public float Value
+            {
+                get {
+                    return outputMode == OutputMode.Normal? value: - value;
+                }
+            }
+
+            public NodeGenes(float _value, Types _type, OperatorType _operatorType=OperatorType.Multiply, OutputMode _outputMode=OutputMode.Normal)
+            {
+                value = _value;
+                type = _type;
+                operatorType = _operatorType;
+                outputMode = _outputMode;
+            }
+
+            public enum Types
+            {
+                Sensor,
+                Output,
+                Hidden,
+            }
+
+            public enum OperatorType
+            {
+                Multiply,
+                Plus,
+            }
+
+            public enum OutputMode
+            {
+                Normal,
+                Reverse
+            }
         }
+
+        public struct ConnectionGenens
+        {
+            public int inputNodeIndex;
+            public int outputNodeIndex;
+            public float weight;
+            public bool enabled;
+
+            public ConnectionGenens(int _inputNodeIndex, int _outputNodeIndex, float _weight, bool _enabled=true)
+            {
+                inputNodeIndex = _inputNodeIndex;
+                outputNodeIndex = _outputNodeIndex;
+                weight = _weight;
+                enabled = _enabled;
+            }
+        }
+        #endregion
     }
-    #endregion
 }
