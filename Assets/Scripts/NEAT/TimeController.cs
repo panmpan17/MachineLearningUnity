892281ec9d325f9a33c77640000f5e2d33caf52b
Michael Pan <panmpan@gmail.com>
2021-04-23 09:11:52 +0800

    Add time control for faster training

---
+++ Assets/Scripts/NEAT/TimeController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public float timeScale;
+
+    public float targetFPS;
+
+    public float timeScaleCap;
+
+    public float recalculateTime;
+    private float recalculateTimer;
+
+    private float fixedDeltaTime;
+
+    private void Start() {
+        fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    void Update()
+    {
+        recalculateTimer += Time.unscaledDeltaTime;
+
+        if (recalculateTimer >= recalculateTime)
+        {
+            recalculateTimer = 0;
+
+            int FPS = Mathf.RoundToInt(1 / Time.deltaTime);
+
+            timeScale = FPS / targetFPS;
+            if (timeScale > timeScaleCap) timeScale = timeScaleCap;
+            else if (timeScale < 0.1f) timeScale = 0.1f;
+
+            Time.timeScale = timeScale;
+            Time.fixedDeltaTime = fixedDeltaTime * Time.timeScale;
+        }
+    }
+}
